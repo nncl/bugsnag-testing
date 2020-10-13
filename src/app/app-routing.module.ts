cba968c06aaea41d706582966fb344d65974a256
Caue Almeida <nncl@live.com>
2020-10-13 10:56:14 -0300

    feat: Criação de rotas de Home e About.
    - About: via lazy loading para simularmos o carregamento de dados pelo servidor via Angular Universal.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+}, { path: 'sobre', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
