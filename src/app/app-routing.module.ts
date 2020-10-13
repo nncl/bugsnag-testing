1917eea7b6618f7bdbffdac6606d8d4034a43795
Caue Almeida <nncl@live.com>
2020-10-13 11:01:05 -0300

    feat: Adicionado Angular Universal
    Agora podemos verificar que o `title` das páginas é bindado no HTML da mesma quando servido diretamente pelo servidor.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,9 @@ const routes: Routes = [{
 }, { path: 'sobre', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabled'
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
